d3e62d70edb9a96315d9a1420e05edf049f43edf
Mistoruu <maille.eliott@gmail.com>
2025-03-17 04:26:52 +0100

    bug fix

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (isset($_POST['add_to_cart'])) {
-    $product_id = $POST['product_id'];
+    $product_id = $_POST['product_id'];
 
     if(!isset($_SESSION['cart'])) {
         $_SESSION['cart'] = [];
